3c5ecfca199c6cad81cc8e516080a08a58eea272
AAFIA ALI <amaafiaali.ofcl@gmail.com>
2026-06-03 17:21:34 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,58 @@
+require('dotenv').config()
+const express=require('express')
+const app=express()
+
+const jwt = require('jsonwebtoken')
+app.use(express.json())
+
+
+const posts = [
+    {
+        username: 'Kyle',
+        iss:'oauth',
+        exp:'1780396113',
+        iat: '1780396053',
+        title: 'Post 1',
+        role: 'admin'
+    },
+    {
+        username: 'Jim',
+        iss:'oauth',
+        exp:'1780396112',
+        iat: '1780396052',
+        title: 'Post 2',
+        role:'viewer'
+    }
+]
+
+app.get('/posts',authenticateToken,(req,res) => {
+    res.json(posts.filter(post=> post.username ===req.user.name))
+})
+
+// app.post('/login',(req,res) => {
+//     //authenticate the user
+//     const username = req.body.username 
+//     const user= {name :username}
+//     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+//     res.json({accessToken: accessToken})
+// })
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token == null) {
+        return res.sendStatus(401)
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) {
+            return res.sendStatus(403)
+        }
+
+        req.user = user
+        next()
+    })
+}
+
+app.listen(3000)
